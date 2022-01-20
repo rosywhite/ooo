d9c9f9ffc3a553faec18b1c6b292c9e89d3d89fb
rosy white <rosy@rosywhite.ooo>
2022-01-19 20:09:30 -0500

    finish slices

--- rust_projects/ch4_examples/src/main.rs
+++ rust_projects/ch4_examples/src/main.rs
@@ -4,6 +4,43 @@ fn main() {
   get_ready_for(&mut saturday);
 
   println!("{}", saturday);
+
+  let my_string = String::from("hello world");
+
+  //let word = first_word(&s); // word will get the value 5
+  //s.clear();
+
+  //println!("{}", word);
+
+  // `first_word` works on slices of `String`s, whether partial or whole
+  let word = first_word(&my_string[0..6]);
+  let word = first_word(&my_string[..]);
+  // `first_word` also works on references to `String`s, which are equivalent
+  // to whole slices of `String`s
+  let word = first_word(&my_string);
+
+  let my_string_literal = "hello world";
+
+  // `first_word` works on slices of string literals, whether partial or whole
+  let word = first_word(&my_string_literal[0..6]);
+  let word = first_word(&my_string_literal[..]);
+
+  // Because string literals *are* string slices already,
+  // this works too, without the slice syntax!
+  let word = first_word(my_string_literal);
+
+}
+
+fn first_word(s: &str) -> &str  {
+  let bytes = s.as_bytes();
+
+  for (i, &item) in bytes.iter().enumerate() {
+      if item == b' ' {
+          return &s[0..i];
+      }
+  }
+
+  &s[..]
 }
 
 fn get_ready_for(some_string: &mut String) {
